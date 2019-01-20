10d4bc8c7174ce91404a3259e4052206aeff015d
SirFloIII <sirfloiii@gmail.com>
2019-01-20 11:16:16 +0100

    main.py, aber es fehlt noch p1 bis p4

--- eulerMarijuana.py
+++ eulerMarijuana.py
@@ -9,62 +9,67 @@ import numpy as np
 import compfy
 #from tqdm import tqdm
 
-#startwerte
-PEP0 = 140
-KO0 = 50 
-vp0 = 10
-vc0 = 10
+def simulate(PEP0, KO0, vp0, vc0, mü, vqp, vqc, sigmap, sigmac, Kp, Kc, pp, pc, p1, p2, p3, p4, N, T):
+    h = T/(N-1)
+    
+    #generierung der korrelierten normalverteilungen
+    var = np.array([[1, pp, p1, p2],
+                    [pp, 1, p3, p4],
+                    [p1, p3, 1, pc],
+                    [p2, p4, pc, 1]])
+    
+    dW = np.random.normal(size = (4,N))
+    dW = np.sqrt(h) * var.dot(dW)
+    
+    #defining a and b for the variance
+    avp = lambda t, v : Kp*(vqp - v)
+    avc = lambda t, v : Kc*(vqc - v)
+    
+    bvp = lambda t, v : sigmap*np.sqrt(v)
+    bvc = lambda t, v : sigmac*np.sqrt(v)
+    
+    vp, _ = compfy.EulerSDE(avp, bvp, vp0, T=T, N=N, dW = dW[1], mode = "absolute")
+    vc, _ = compfy.EulerSDE(avc, bvc, vc0, T=T, N=N, dW = dW[3], mode = "absolute")
+    
+    #defing a and b for the stocks themself
+    aPEP = lambda t, S : mü * S
+    aKO = lambda t, S : mü * S
+    
+    i = lambda t : int(t/h)
+    bPEP = lambda t, S : np.sqrt(vp[i(t)])
+    bKO = lambda t, S : np.sqrt(vc[i(t)])
+    
+    PEP, _ = compfy.EulerSDE(aPEP, bPEP, PEP0, T=T, N=N, dW = dW[0])
+    KO, _ = compfy.EulerSDE(aKO, bKO, KO0, T=T, N=N, dW = dW[2])
+    
+    return PEP, KO
 
-#konstanten
-mü = 0.01
-vqp = 10
-vqc = 11
-sigmap = 3
-sigmac = 2
-K = 0.1
+if __name__ == "__main__":
+        
+    #startwerte
+    PEP0 = 140
+    KO0 = 50 
+    vp0 = 10
+    vc0 = 10
+    
+    #konstanten
+    mü = 0.01
+    vqp = 10
+    vqc = 11
+    sigmap = 3
+    sigmac = 2
+    Kp = 0.1
+    Kc = 0.1
+    
+    #korrelationskoeffizenten
+    pp = 0.5
+    pc = 1
+    p1, p2, p3, p4 = [1,2,3,4]
+    
+    #simulationsdetails
+    N = 1000
+    T = 20
 
-#korrelationskoeffizenten
-pp = 0.5
-pc = 1
-p1, p2, p3, p4 = [1,2,3,4]
-
-#simulationsdetails
-N = 1000
-T = 20
-h = T/(N-1)
-
-#generierung der korrelierten normalverteilungen
-var = np.array([[1, pp, p1, p2],
-                [pp, 1, p3, p4],
-                [p1, p2, 1, pc],
-                [p2, p4, pc, 1]])
-
-dW = np.random.normal(size = (4,N))
-dW = np.sqrt(h) * var.dot(dW)
-
-#defining a and b for the variance
-avp = lambda t, v : K*(vqp - v)
-avc = lambda t, v : K*(vqc - v)
-
-bvp = lambda t, v : sigmap*np.sqrt(v)
-bvc = lambda t, v : sigmac*np.sqrt(v)
-
-vp, _ = compfy.EulerSDE(avp, bvp, vp0, T=T, N=N, dW = dW[1], mode = "absolute")
-vc, _ = compfy.EulerSDE(avc, bvc, vc0, T=T, N=N, dW = dW[3], mode = "absolute")
-
-#defing a and b for the stocks themself
-aPEP = lambda t, S : mü * S
-aKO = lambda t, S : mü * S
-
-i = lambda t : int(t/h)
-bPEP = lambda t, S : np.sqrt(vp[i(t)])
-bKO = lambda t, S : np.sqrt(vc[i(t)])
-
-PEP, _ = compfy.EulerSDE(aPEP, bPEP, PEP0, T=T, N=N, dW = dW[0])
-KO, _ = compfy.EulerSDE(aKO, bKO, KO0, T=T, N=N, dW = dW[2])
-
-
-
-
+    PEP, KO = simulate(PEP0, KO0, vp0, vc0, mü, vqp, vqc, sigmap, sigmac, Kp, Kc, pp, pc, p1, p2, p3, p4, N, T)
 
 
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import Heston_calibration
 import eulerMarijuana
 
 import numpy as np
+import datetime
+from time import time
 
 dataPEP = optionsData.getOptionDataFromYahoo("PEP")
 dataKO = optionsData.getOptionDataFromYahoo("KO")
@@ -24,3 +26,16 @@ KO0 = optionsData.getCurrentPrice("KO")
 thetaKO, _ = Heston_calibration.LevMarquCali(theta, KO0, mü, dataKO)
 
 #theta=(v0,vbar,rho,kap,sigma)
+
+v0p, vqp, rhop, kapp, sigmap = thetaPEP
+v0c, vqc, rhoc, kapc, sigmac = thetaKO
+
+N = 1000
+#expiration dates: 1.2.2019, 15.2.2019, 21.6.2019
+now = time()
+expdates = ["2019-02-01", "2019-02-15", "2019-06-21"]
+Ts = [(datetime.datetime.strptime(expdate, "%Y-%m-%d").timestamp()-now)/60/60/24/356 for expdate in expdates]
+
+
+
+PEP, KO = eulerMarijuana.simulate(PEP0, KO0, v0p, v0c, mü, vqp, vqc, sigmap, sigmac, kapp, kapc, rhop, rhoc, p1, p2, p3, p4, N, Ts[0])
