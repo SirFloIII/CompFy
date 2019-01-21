cde66e19e00a25b00de7bab704beb90e74a76d4d
wagi013 <46620625+wagi013@users.noreply.github.com>
2019-01-21 19:51:39 +0100

    Selbsterklärend

---
+++ getzeugs.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 21 18:00:43 2019
+
+@author: Thomas
+"""
+
+import optionsData as op
+import numpy
+
+Cola=op.getOptionDataFromYahoo("KO")
+Pepsi=op.getOptionDataFromYahoo("PEP")
+
+
+numpy.savetxt('Cola1.csv', Cola, fmt='%f', delimiter=';')
+numpy.savetxt('Pepsi.csv',Pepsi,fmt='%f',delimiter=';')
+
+
+
+
+
