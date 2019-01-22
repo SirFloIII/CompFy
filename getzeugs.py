8ab35a76515a13f01aef1da5b3a0b0ff21432f9a
wagi013 <wagi013@yahoo.de>
2019-01-22 17:17:20 +0100

    .

--- getzeugs.py
+++ getzeugs.py
@@ -10,11 +10,19 @@ import numpy
 
 Cola=op.getOptionDataFromYahoo("KO")
 Pepsi=op.getOptionDataFromYahoo("PEP")
+IBM=op.getOptionDataFromYahoo("IBM")
+Intel=op.getOptionDataFromYahoo("INTC")
+NVIDIA=op.getCurrentPrice("NVDA")
+Google=op.getCurrentPrice("GOOG")
+Apple=op.getOptionDataFromYahoo("AAPL")
+XLK=op.getOptionDataFromYahoo("XLK")
 
 
 numpy.savetxt('Cola.csv', Cola, fmt='%f', delimiter=';')
 numpy.savetxt('Pepsi.csv',Pepsi,fmt='%f',delimiter=';')
-
+numpy.savetxt('IBM.csv',IBM,fmt='%f',delimiter=';')
+numpy.savetxt('Intel.csv',Intel,,fmt='%f',delimiter=';')
+numpy.savetxt('NVDIA.csv',NVIDIA,fmt='%f',delimiter=';')
 
 
 
