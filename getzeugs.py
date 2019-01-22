707c1b980631ac0dbb7ab4b8e37cba72ef907788
SirFloIII <sirfloiii@gmail.com>
2019-01-22 18:05:39 +0100

    added filtering

--- getzeugs.py
+++ getzeugs.py
@@ -11,8 +11,12 @@ import numpy
 symbols = ["PEP", "KO", "IBM", "INTC", "NVDA", "GOOG", "AAPL", "XLK"]
 
 for s in symbols:
+    
+    S0 = op.getCurrentPrice(s)
+    numpy.savetxt("S0_"+s+".csv", [S0])
+    
     data = op.getOptionDataFromYahoo(s)
+    data = data[data[:,0] < 2*S0]
     numpy.savetxt("KTP_"+s+".csv", data, fmt = "%f", delimiter = ";")
     
-    S0 = [op.getCurrentPrice(s)]
-    numpy.savetxt("S0_"+s+".csv", S0)
+    
